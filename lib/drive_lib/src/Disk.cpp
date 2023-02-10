1770c3a4ceeb7242650f01a5e3d161b4063320af
Dmitry Morozov <9984398@gmail.com>
2023-02-11 02:12:49 +0300

    FileSystem
    Format () : Debug
    
    15 %
    
    inode : 0
    
    Tests : Failed

--- lib/drive_lib/src/Disk.cpp
+++ lib/drive_lib/src/Disk.cpp
@@ -5,175 +5,175 @@
 #include "Disk.hpp"
 
 #include <array>
+#include <filesystem>
 #include <iostream>
 #include <stdexcept>
 #include <sstream>
 
 void Disk::open(const std::string &path, size_t n_blocks) {
 
-  if (n_blocks < 2) throw std::runtime_error("Disk size must be at least 2 blocks");
+	if (n_blocks < 2) throw std::runtime_error("Disk size must be at least 2 blocks");
 
-  show_file_permissions(path);
+	show_file_permissions(path);
 
-  FileDescriptor_.open(path, std::ios::in | std::ios::out | std::ios::binary);
+	FileDescriptor_.open(path, std::ios::in | std::ios::out | std::ios::binary);
 
-  if ((!FileDescriptor_.is_open()) or (!FileDescriptor_.good())) {
-    std::stringstream ss;
-    ss << "Failed to open disk image: " << path << " : " << strerror(errno);
-    throw std::runtime_error(ss.str());
-  }
+	if ((!FileDescriptor_.is_open()) or (!FileDescriptor_.good())) {
+		std::stringstream ss;
+		ss << "Failed to open disk image: " << path << " : " << strerror(errno);
+		throw std::runtime_error(ss.str());
+	}
 
-  //Reserve space for the disk
-  FileDescriptor_.seekp(static_cast<long>(n_blocks * BLOCK_SIZE), std::ios::beg);
-  FileDescriptor_.write("", 1);
-  FileDescriptor_.seekp(0, std::ios::beg);
+	//Reserve space for the disk
 
-  blocks_ = n_blocks;
-  reads_ = 0;
-  writes_ = 0;
+	std::filesystem::resize_file(path, n_blocks*BLOCK_SIZE); //Call to resize_file instead of using ftruncate
+
+	blocks_ = n_blocks;
+	reads_ = 0;
+	writes_ = 0;
 
 }
 
 Disk::~Disk() {
 
-  if (FileDescriptor_.is_open()) {
+	if (FileDescriptor_.is_open()) {
 
-    std::cout << "Disk: " << reads_ << " reads, " << writes_ << " writes" << std::endl;
+		std::cout << "Disk: " << reads_ << " reads, " << writes_ << " writes" << std::endl;
 
-    FileDescriptor_.close();
+		FileDescriptor_.close();
 
-  }
+	}
 
 }
 
 void Disk::ValidCheck(int block_num, const std::array<std::byte, Disk::BLOCK_SIZE> &data) const {
 
-  std::stringstream ss;
+	std::stringstream ss;
 
-  if (block_num < 0) {
-    ss << "Invalid block number: " << block_num;
-    throw std::invalid_argument(ss.str());
-  }
+	if (block_num < 0) {
+		ss << "Invalid block number: " << block_num;
+		throw std::invalid_argument(ss.str());
+	}
 
-  if (block_num >= blocks_) {
-    ss << "Block number " << block_num << " is out of range";
-    throw std::invalid_argument(ss.str());
-  }
+	if (block_num >= blocks_) {
+		ss << "Block number " << block_num << " is out of range";
+		throw std::invalid_argument(ss.str());
+	}
 
-  if (data.size() != BLOCK_SIZE or data.empty()) {
-    ss << "Invalid data buffer";
-    throw std::invalid_argument(ss.str());
-  }
+	if (data.size()!=BLOCK_SIZE or data.empty()) {
+		ss << "Invalid data buffer";
+		throw std::invalid_argument(ss.str());
+	}
 
 }
 
 void Disk::read(int block_num, std::array<std::byte, Disk::BLOCK_SIZE> &data) {
 
-  ValidCheck(block_num, data);
+	ValidCheck(block_num, data);
 
-  if (FileDescriptor_.seekg(block_num * static_cast<int>(BLOCK_SIZE), std::ios::beg).fail()) {
+	if (FileDescriptor_.seekg(block_num*static_cast<int>(BLOCK_SIZE), std::ios::beg).fail()) {
 
-    std::stringstream ss;
-    ss << "Unable to seek " << block_num << ": " << strerror(errno);
-    throw std::runtime_error(ss.str());
+		std::stringstream ss;
+		ss << "Unable to seek " << block_num << ": " << strerror(errno);
+		throw std::runtime_error(ss.str());
 
-  }
+	}
 
-  if (FileDescriptor_.eof()) {
-    std::stringstream ss;
-    ss << "Unable to read (EOF Position) " << block_num << ": " << strerror(errno);
-    throw std::runtime_error(ss.str());
-  }
+	if (FileDescriptor_.eof()) {
+		std::stringstream ss;
+		ss << "Unable to read (EOF Position) " << block_num << ": " << strerror(errno);
+		throw std::runtime_error(ss.str());
+	}
 
-  if (FileDescriptor_.bad()) {
-    std::stringstream ss;
-    ss << "Unable to read (BAD descriptor) " << block_num << ": " << strerror(errno);
-    throw std::runtime_error(ss.str());
-  }
+	if (FileDescriptor_.bad()) {
+		std::stringstream ss;
+		ss << "Unable to read (BAD descriptor) " << block_num << ": " << strerror(errno);
+		throw std::runtime_error(ss.str());
+	}
 //Read the data from the disk
-  FileDescriptor_.read(reinterpret_cast<char *>(data.data()), Disk::BLOCK_SIZE);
+	FileDescriptor_.read(reinterpret_cast<char *>(data.data()), Disk::BLOCK_SIZE);
 //FIXME:
-  if (FileDescriptor_.fail()) {
-    std::stringstream ss;
-    ss << "Unable to read " << block_num << ": " << strerror(errno);
-    throw std::runtime_error(ss.str());
-  }
+	if (FileDescriptor_.fail()) {
+		std::stringstream ss;
+		ss << "Unable to read " << block_num << ": " << strerror(errno);
+		throw std::runtime_error(ss.str());
+	}
 
-  ++reads_;
+	++reads_;
 
 }
 
 void Disk::write(int block_num, const std::array<std::byte, Disk::BLOCK_SIZE> &data) {
 
-  // FIXME: Exception  is thrown here
-  ValidCheck(block_num, data);
+	// FIXME: Exception  is thrown here
+	ValidCheck(block_num, data);
 
-  if (FileDescriptor_.seekp(block_num * static_cast<int>(BLOCK_SIZE), std::ios::beg).fail()) {
+	if (FileDescriptor_.seekp(block_num*static_cast<int>(BLOCK_SIZE), std::ios::beg).fail()) {
 
-    std::stringstream ss;
-    ss << "Unable to seek " << block_num << ": " << strerror(errno);
-    throw std::runtime_error(ss.str());
+		std::stringstream ss;
+		ss << "Unable to seek " << block_num << ": " << strerror(errno);
+		throw std::runtime_error(ss.str());
 
-  }
+	}
 
-  if (FileDescriptor_.bad()) {
-    std::stringstream ss;
-    ss << "Unable to write (BAD descriptor) " << block_num << ": " << strerror(errno);
-    throw std::runtime_error(ss.str());
-  }
+	if (FileDescriptor_.bad()) {
+		std::stringstream ss;
+		ss << "Unable to write (BAD descriptor) " << block_num << ": " << strerror(errno);
+		throw std::runtime_error(ss.str());
+	}
 
-  FileDescriptor_.write(reinterpret_cast<const char *>(data.data()), Disk::BLOCK_SIZE);
+	FileDescriptor_.write(reinterpret_cast<const char *>(data.data()), Disk::BLOCK_SIZE);
 
-  if (FileDescriptor_.fail()) {
-    std::stringstream ss;
-    ss << "Unable to write " << block_num << ": " << strerror(errno);
-    throw std::runtime_error(ss.str());
-  }
-  //Flush the buffer to the disk
-  FileDescriptor_.flush();
+	if (FileDescriptor_.fail()) {
+		std::stringstream ss;
+		ss << "Unable to write " << block_num << ": " << strerror(errno);
+		throw std::runtime_error(ss.str());
+	}
+	//Flush the buffer to the disk
+	FileDescriptor_.flush();
 
-  ++writes_;
+	++writes_;
 
 }
 
 void Disk::close() {
 
-  if (FileDescriptor_.is_open()) {
+	if (FileDescriptor_.is_open()) {
 
-    std::cout << "Disk: " << reads_ << " reads, " << writes_ << " writes" << std::endl;
+		std::cout << "Disk: " << reads_ << " reads, " << writes_ << " writes" << std::endl;
 
-    reads_ = 0;
-    writes_ = 0;
+		reads_ = 0;
+		writes_ = 0;
 
-    FileDescriptor_.close();
+		FileDescriptor_.close();
 
-  }
+	}
 
 }
 Disk::Disk() : FileDescriptor_(nullptr), blocks_(0), reads_(0), writes_(0), mounts_(0) {}
 
 Disk::Disk(std::string path, std::size_t n_blocks) {
 
-  open(path, n_blocks);
+	open(path, n_blocks);
 
 }
 
 void Disk::show_file_permissions(const std::filesystem::path &path) const {
-  using std::filesystem::perms;
+	using std::filesystem::perms;
 
-  perms p = std::filesystem::status(path).permissions();
+	perms p = std::filesystem::status(path).permissions();
 
-  auto show = [=](char op, perms perm) {
-    std::cout << (perms::none == (perm & p) ? '-' : op);
-  };
-  show('r', perms::owner_read);
-  show('w', perms::owner_write);
-  show('x', perms::owner_exec);
-  show('r', perms::group_read);
-  show('w', perms::group_write);
-  show('x', perms::group_exec);
-  show('r', perms::others_read);
-  show('w', perms::others_write);
-  show('x', perms::others_exec);
-  std::cout << '\n';
+	auto show = [=](char op, perms perm) {
+	  std::cout << (perms::none==(perm & p) ? '-' : op);
+	};
+	show('r', perms::owner_read);
+	show('w', perms::owner_write);
+	show('x', perms::owner_exec);
+	show('r', perms::group_read);
+	show('w', perms::group_write);
+	show('x', perms::group_exec);
+	show('r', perms::others_read);
+	show('w', perms::others_write);
+	show('x', perms::others_exec);
+	std::cout << '\n';
 }
--- tests/src/fs_test.cpp
+++ tests/src/fs_test.cpp
@@ -38,47 +38,26 @@ TEST(FS, Format_Test) {
 	EXPECT_EQ(fs.get_cur_disk()->mounted(), false);
 
 	EXPECT_NO_THROW(FileSystem::format(fs.get_cur_disk()));
-//
-//	EXPECT_EQ(fs.get_cur_disk()->mounted(), true);
-//
-//  EXPECT_EQ(fs.get_cur_disk()->size(), 2);
-//  EXPECT_EQ(fs.get_cur_disk()->mounted(), true);
-//  EXPECT_EQ(fs.mounted(), true);
+
 
 	//Check each block
 
-//  std::array<std::byte, Disk::BLOCK_SIZE> data = {};
-//
-//  EXPECT_NO_THROW(fs.get_cur_disk()->read(0, data));
-//
-//  EXPECT_NO_THROW(fs.get_cur_disk()->read(1, data));
-//
-//  EXPECT_THROW(fs.get_cur_disk()->read(2, data), std::invalid_argument);
-//
-//  EXPECT_THROW(fs.get_cur_disk()->read(3, data), std::invalid_argument);
-//
-//  //Check each block for validity
-//
-//  EXPECT_NO_THROW(fs.get_cur_disk()->isValid(0, data));
-//
-//  EXPECT_NO_THROW(fs.get_cur_disk()->isValid(1, data));
-//
-//  EXPECT_THROW(fs.get_cur_disk()->isValid(2, data), std::invalid_argument);
-//
-//  EXPECT_THROW(fs.get_cur_disk()->isValid(3, data), std::invalid_argument);
+	std::array<std::byte, Disk::BLOCK_SIZE> data = {};
 
 }
 
 TEST(FS, Mount_Test) {
 // Using image.5.img as a test disk
 	EXPECT_EQ(true, true);
-//  std::shared_ptr<Disk> disk = std::make_shared<Disk>();
-//  disk->open("image.5.img", 5);
-//
-//  FileSystem fs;
-//  fs.mount(disk);
-//
-//  EXPECT_EQ(fs.get_cur_disk()->mounted(), true);
+	std::shared_ptr<Disk> disk = std::make_shared<Disk>();
+	disk->open("image.5.img", 5);
+
+	FileSystem fs;
+
+	fs.set_cur_disk(disk);
+	fs.mount(disk);
+
+	EXPECT_EQ(fs.get_cur_disk()->mounted(), true);
 
 }
 
